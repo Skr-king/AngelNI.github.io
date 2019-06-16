9d99b187086e5f47acddadbb12a94b95eeecc62a
AngelNI <1476879474@qq.com>
2019-06-16 19:21:36 +0800

    Site updated: 2019-06-16 19:21:34

--- js/src/snow.js
+++ js/src/snow.js
@@ -6,9 +6,9 @@
 	documentWidth	= $(document).width(),
 	defaults = {
 		minSize		: 10,
-		maxSize		: 30,
+		maxSize		: 20,
 		newOn		: 1000,
-		flakeColor	: "#FFFFFF" /* 此处可以定义雪花颜色，若要白色可以改为#FFFFFF */
+		flakeColor	: "#AFDAEF" /* 此处可以定义雪花颜色，若要白色可以改为#FFFFFF */
 	},
 	options	= $.extend({}, defaults, options);
 	var interval= setInterval( function(){
@@ -35,8 +35,9 @@
 })(jQuery);
 $(function(){
     $.fn.snow({ 
-	    minSize: 10, /* 定义雪花最小尺寸 */
-	    maxSize: 60,/* 定义雪花最大尺寸 */
-	    newOn: 200  /* 定义密集程度，数字越小越密集 */	
+	    minSize: 5, /* 定义雪花最小尺寸 */
+	    maxSize: 50,/* 定义雪花最大尺寸 */
+	    newOn: 300  /* 定义密集程度，数字越小越密集 */
     });
 });
+
